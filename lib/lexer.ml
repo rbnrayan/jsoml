274790845df2709dc051c9e323832d3fa7836681
rbnrayan <rayan.robion7@gmail.com>
2024-06-20 22:52:46 +0200

    added bools lexing

--- lib/lexer.ml
+++ lib/lexer.ml
@@ -19,23 +19,24 @@ and get_next_token l =
     | ']' -> (Some Right_bracket, sub)
     | ':' -> (Some Colon, sub)
     | ',' -> (Some Comma, sub)
-    | '"' ->
-        let tok, l = get_next_str_token sub in
-        (Some tok, l)
-    | ch when is_numeric ch ->
-        let tok, l = get_next_number_token l in
-        (Some tok, l)
+    | '"' -> get_next_str_token sub
+    | ch when is_numeric ch -> get_next_number_token l
     | ch when is_whitespace ch -> get_next_token sub
-    | _ -> failwith "Unknown token"
+    | _ -> (
+        match get_next_boolean_token l with
+        | Some tok, sub -> (Some tok, sub)
+        | None, _ -> failwith "Unknown token")
 
 and get_next_str_token l =
-  let str =
-    String.to_seq l |> Seq.take_while (fun ch -> ch != '"') |> String.of_seq
-  in
-  let length = String.length l in
-  let str_length = String.length str in
-  let sub = String.sub l (str_length + 1) (length - (str_length + 1)) in
-  (Token.Str str, sub)
+  if String.index_opt l '"' = None then failwith "Unmatched quote for Token.Str"
+  else
+    let str =
+      String.to_seq l |> Seq.take_while (fun ch -> ch != '"') |> String.of_seq
+    in
+    let length = String.length l in
+    let str_length = String.length str in
+    let sub = String.sub l (str_length + 1) (length - (str_length + 1)) in
+    (Some (Token.Str str), sub)
 
 and get_next_number_token l =
   let number =
@@ -44,10 +45,30 @@ and get_next_number_token l =
   let length = String.length l in
   let number_length = String.length number in
   let sub = String.sub l number_length (length - number_length) in
-  (Token.Number (int_of_string number), sub)
+  (Some (Token.Number (int_of_string number)), sub)
+
+and get_next_boolean_token l =
+  let is_whitespace_or_comma ch = is_whitespace ch || ch = ',' in
+  let s =
+    String.to_seq l
+    |> Seq.take_while (fun ch -> not (is_whitespace_or_comma ch))
+    |> String.of_seq
+  in
+  let sub =
+    String.sub l (String.length s) (String.length l - String.length s)
+  in
+  if is_boolean s then (Some (Token.Boolean (string_to_bool s)), sub)
+  else (None, l)
 
 and is_whitespace = function ' ' | '\r' | '\n' | '\t' -> true | _ -> false
 
 and is_numeric = function
   | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0' -> true
   | _ -> false
+
+and is_boolean = function "true" | "false" -> true | _ -> false
+
+and string_to_bool = function
+  | "true" -> true
+  | "false" -> false
+  | _ -> failwith "unexpected input, cannot convert it to bool"
