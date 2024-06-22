f1b06ca80335479489b25eaf8e84f526de1c63a7
rbnrayan <rayan.robion7@gmail.com>
2024-06-23 00:09:03 +0200

    handle null value

--- bin/main.ml
+++ bin/main.ml
@@ -3,5 +3,4 @@ open Jsoml
 let () =
   let json_ic = open_in "./bin/in.json" in
   let lexer = Lexer.from_in_channel json_ic in
-  Lexer.tokenize lexer |> Parser.make |> Parser.parse |> Parser.ast_to_string
-  |> print_endline
+  Lexer.tokenize lexer |> List.map Token.show |> List.iter print_endline
--- lib/lexer.ml
+++ lib/lexer.ml
@@ -13,62 +13,58 @@ and get_next_token l =
     let sub = String.sub l 1 (String.length l - 1) in
     let open Token in
     match ch with
+    (* Skip whitespaces *)
+    | ch when is_whitespace ch -> get_next_token (String.trim sub)
     | '{' -> (Some Left_brace, sub)
     | '}' -> (Some Right_brace, sub)
     | '[' -> (Some Left_bracket, sub)
     | ']' -> (Some Right_bracket, sub)
     | ':' -> (Some Colon, sub)
     | ',' -> (Some Comma, sub)
-    | '"' -> get_next_str_token sub
-    | ch when is_numeric ch -> get_next_number_token l
-    | ch when is_whitespace ch -> get_next_token sub
-    | _ -> (
-        match get_next_boolean_token l with
-        | (Some _, _) as res -> res
-        | None, _ -> failwith "Unknown token")
+    | _ -> get_next_literal_token l
 
-and get_next_str_token l =
-  if String.index_opt l '"' = None then failwith "Unmatched quote for Token.Str"
-  else
-    let str =
-      String.to_seq l |> Seq.take_while (fun ch -> ch != '"') |> String.of_seq
-    in
-    let length = String.length l in
-    let str_length = String.length str in
-    let sub = String.sub l (str_length + 1) (length - (str_length + 1)) in
-    (Some (Token.Str str), sub)
-
-and get_next_number_token l =
-  let number =
-    String.to_seq l |> Seq.take_while (fun ch -> is_numeric ch) |> String.of_seq
-  in
-  let length = String.length l in
-  let number_length = String.length number in
-  let sub = String.sub l number_length (length - number_length) in
-  (Some (Token.Number (int_of_string number, None)), sub)
-
-and get_next_boolean_token l =
-  let is_whitespace_or_comma ch = is_whitespace ch || ch = ',' in
-  let s =
-    String.to_seq l
-    |> Seq.take_while (fun ch -> not (is_whitespace_or_comma ch))
-    |> String.of_seq
-  in
-  let sub =
-    String.sub l (String.length s) (String.length l - String.length s)
-  in
-  if is_boolean s then (Some (Token.Boolean (string_to_bool s)), sub)
-  else (None, l)
+(*
+  Literals:
+  - String : "literal"
+  - Number : 1234
+  - Boolean: true | false
+  - Null   : null
+*)
+and get_next_literal_token l =
+  let get_literal l p = String.to_seq l |> Seq.take_while p |> String.of_seq in
+  let ch = String.get l 0 in
+  let open Token in
+  match ch with
+  | '"' -> (
+      let quote_idx = String.index_from_opt l 1 '"' in
+      match quote_idx with
+      | Some idx ->
+          let str = String.sub l 1 (idx - 1) in
+          let sub = String.sub l (idx + 1) (String.length l - idx - 1) in
+          (Some (Str str), sub)
+      | None -> failwith "Unmatched quote for Token.Str")
+  | ch when is_numeric ch ->
+      let number = get_literal l (fun ch -> is_numeric ch) in
+      let sub =
+        String.sub l (String.length number)
+          (String.length l - String.length number)
+      in
+      (Some (Number (int_of_string number, None)), sub)
+  | _ -> (
+      let literal =
+        get_literal l (fun ch -> not (ch = ',' || is_whitespace ch))
+      in
+      let sub =
+        String.sub l (String.length literal)
+          (String.length l - String.length literal)
+      in
+      match literal with
+      | "null" -> (Some Null, sub)
+      | "true" | "false" -> (Some (Boolean (bool_of_string literal)), sub)
+      | _ -> failwith (Printf.sprintf "Unknown literal `%s`\n" literal))
 
 and is_whitespace = function ' ' | '\r' | '\n' | '\t' -> true | _ -> false
 
 and is_numeric = function
   | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0' -> true
   | _ -> false
-
-and is_boolean = function "true" | "false" -> true | _ -> false
-
-and string_to_bool = function
-  | "true" -> true
-  | "false" -> false
-  | _ -> failwith "unexpected input, cannot convert it to bool"
